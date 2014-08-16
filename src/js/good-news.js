d5318b33b5d6b33041c5108c6df9be3edafd4a4a
Sung Kim <jstkim7@gmail.com>
2014-08-16 01:19:54 -0400

    Add rough basic style and animation

--- src/js/good-news.js
+++ src/js/good-news.js
@@ -1,3 +1,42 @@
 (function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  var cancelRAF = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame;
+
+  var goodNewsBar = document.getElementById("good-news");
+  var closeButton = document.getElementById("close-good-news-bar");
+
+  var y = -30;
+  var requestId;
+
+  function sayHello() {
+    requestId = requestAnimationFrame(sayHello);
+    goodNewsBar.style.top = y + "px";
+    //goodNewsBar.style.marginTop = "30px";
+    console.log(y + "px");
+    if (y < 0) {
+      y+=2;
+    } else {
+      cancelRAF(requestId)
+    }
+  }
+
+  sayHello();
+
+  function sayGoodbye() {
+    requestId = requestAnimationFrame(sayGoodbye);
+    goodNewsBar.style.top = y + "px";
+    //goodNewsBar.style.marginTop = "30px";
+    console.log(y + "px");
+    if (y > -30) {
+      y-=2;
+    } else {
+      cancelRAF(requestId)
+    }
+  }
+
   
+  console.log(closeButton);
+  closeButton.onclick = function() {
+    sayGoodbye();
+  }
 })();
